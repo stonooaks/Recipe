aa7181ed9f7efc80a2dec2ff41b1db62a0264e0e
Michael <stonooak@gmail.com>
2015-01-12 20:05:59 -0500

    Added other projects

---
+++ Recipe.Data/Recipe.Data/RecipeContext.cs
@@ -0,0 +1,28 @@
+﻿using Hughes.DbContexts;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe.Data {
+
+
+
+    /// <summary>
+    /// Recipe Context for the Recipe Database
+    /// </summary>
+    /// <remarks>
+    /// It inherits the DbContextBase from the Hughes Library
+    /// </remarks>
+    public class RecipeContext : DbContextBase
+    {
+        public DbSet<Direction> Directions { get; set; }
+        public DbSet<MenuItem> MenuItems { get; set; }
+        public DbSet<Ingrediant> Ingrediants { get; set; }
+        public DbSet<IngrediantMeasurement> IngrediantMeasurements { get; set; }
+        public DbSet<Recipe> Recipes { get; set; }
+
+    }
+}
---
+++ Recipe.Data/Recipe.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Recipe.Web.MvcApplication" Language="C#" %>
